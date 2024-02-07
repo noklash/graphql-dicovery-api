fac5526d961fa08ea239f6602ba4d44c39fb7455
noklash <123834419+noklash@users.noreply.github.com>
2024-02-07 11:55:47 +0100

    Update post.js
    removed comment

--- src/models/post.js
+++ src/models/post.js
@@ -4,7 +4,7 @@ const PostSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true } // Change type to ObjectId and add ref to User model
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true } 
 });
 
 const Post = model('Post', PostSchema); 
